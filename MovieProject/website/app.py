3a8f353d147e0f9dfcb1ce9e037d697e038b9db7
tariklhousni <tarik.lhousni@hotmail.com>
2025-04-16 13:05:42 -0400

    Update comments to help in the presentation.

--- MovieProject/website/app.py
+++ MovieProject/website/app.py
@@ -4,10 +4,10 @@ import os
 
 app = Flask(__name__)
 
-# This function connects to the database
+# connecting the database to the function
 def get_db_connection():
     base_dir = os.path.dirname(__file__)  # Folder where this file is saved
-    db_path = os.path.join(base_dir, '..', 'database', 'movies.db')  # Go to the database folder
+    db_path = os.path.join(base_dir, '..', 'database', 'movies.db')  # find the path to the database folder
     db_path = os.path.abspath(db_path)  # Get full path to the file
 
     conn = sqlite3.connect(db_path)  # Connect to the database
@@ -19,21 +19,21 @@ def get_db_connection():
 def home():
     return render_template("index.html")
 
-# Show list of movies (converted to dictionary for .items() in HTML)
+# Show list of movies
 @app.route('/movie_list')
 def show_movies():
     conn = get_db_connection()
     rows = conn.execute("SELECT * FROM movies").fetchall()
-    movies = [dict(row) for row in rows]  # Convert sqlite3.Row to dictionary
+    movies = [dict(row) for row in rows]  # Convert sqlite3.Row to dictionary to run the trailer link
     conn.close()
     return render_template("movie_list.html", movies=movies)
 
-# Show movie screening times (converted to dictionary for .items() in HTML)
+# Show movie screening times
 @app.route('/screenings')
 def show_screenings():
     conn = get_db_connection()
     rows = conn.execute("SELECT * FROM screenings").fetchall()
-    screenings = [dict(row) for row in rows]
+    screenings = [dict(row) for row in rows] # Convert sqlite3.Row to dictionary to keep the same type of coding
     conn.close()
     return render_template("screenings.html", screenings=screenings)
 
